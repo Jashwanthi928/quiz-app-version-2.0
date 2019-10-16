276cf725199c11af095bbb1f2b73146cfd167e50
Jashwanthi928 <54614090+Jashwanthi928@users.noreply.github.com>
2019-10-16 16:36:19 +0530

    url is exported

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express =require('express');
 const mongoclient=require('mongodb').mongoclient;
+const url=require('./config/config.js').url;
 
 const app=express();
+
+mongoclient.connect(url);
 
 const port=8000;
 app.listen(port,(req,res)=>{
     console.log('server is running in port 8000');
 });
+
