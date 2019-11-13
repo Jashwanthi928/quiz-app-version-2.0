da710fb8bd51b7a8a912fee80d28f5887bdaeda3
Jashwanthi928 <54614090+Jashwanthi928@users.noreply.github.com>
2019-11-13 12:28:27 +0530

    query to find questions

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,1 +1,17 @@
 const express=require('express');
+const router =express.Router();
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+const questions=require('../models/questions');
+
+
+router.get('/',(req,res)=>{
+questions.find({})
+                    .then(question=>{
+                        if(question)
+                        return res.json(question);
+                        return res.status(404)
+                    })
+                    .catch(err=>console.log('Error'+err));
+})
+module.exports=router;
