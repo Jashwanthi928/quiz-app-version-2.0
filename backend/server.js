91cc4c7f4a64e0fbc51e66deb9b2975a7fe9945a
Jashwanthi928 <54614090+Jashwanthi928@users.noreply.github.com>
2019-11-13 12:29:54 +0530

    setting server to port 8000

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,17 @@
 const express =require('express');
 const mongoose=require('mongoose');
 const url=require('./config/config.js').url;
-
+const questions=require('./routes/routes');
+const cors=require('cors');
+  
+const app=express();
+mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true})
+                    .then(()=>console.log("Mongodb connected"))
+                    .catch(()=>console.log("Error connecting mongodb"))
+const port=process.env.port || 8000;
+app.listen(port,(req,res)=>{
+    console.log('server is running in port 8000');
+});
+app.use('/questions',questions);
+app.use(cors());
 
