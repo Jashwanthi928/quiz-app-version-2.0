62e5f902cc66b579fa2361bf71fb762117d5b365
Jashwanthi928 <54614090+Jashwanthi928@users.noreply.github.com>
2019-11-19 12:04:04 +0530

    using environment variables for port

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,15 @@ const mongoose=require('mongoose');
 const url=require('./config/config.js').url;
 const questions=require('./routes/routes');
 const cors=require('cors');
-  
 const app=express();
+app.use(cors());
 mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true})
                     .then(()=>console.log("Mongodb connected"))
                     .catch(()=>console.log("Error connecting mongodb"))
-const port=process.env.port || 8000;
+const port=8000;
 app.listen(port,(req,res)=>{
     console.log('server is running in port 8000');
 });
 app.use('/questions',questions);
-app.use(cors());
+
 
