c04c7300871a01296f1d8696d5a9f98bf7d6d28d
Jashwanthi928 <54614090+Jashwanthi928@users.noreply.github.com>
2019-11-26 22:36:03 +0530

    it is parsed to convert string to number

--- frontend/js/questions.js
+++ frontend/js/questions.js
@@ -4,6 +4,10 @@ let timer = ()=>{
  let interval = setInterval(() =>{
 
  let timer = timer2.split(':');
+ //by parsing integer,we avoid all extra string processing
+ let minutes = parseInt(timer[0], 10);
+ let seconds = parseInt(timer[1], 10);
+
 }
    
    
