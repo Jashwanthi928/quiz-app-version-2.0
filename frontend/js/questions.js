46735c9202b0f71146b6ba7a13bb72efe44fb569
Jashwanthi928 <54614090+Jashwanthi928@users.noreply.github.com>
2019-11-26 22:36:46 +0530

    if condition

--- frontend/js/questions.js
+++ frontend/js/questions.js
@@ -7,7 +7,12 @@ let timer = ()=>{
  //by parsing integer,we avoid all extra string processing
  let minutes = parseInt(timer[0], 10);
  let seconds = parseInt(timer[1], 10);
-
+ --seconds;
+ minutes = (seconds < 0) ? --minutes : minutes;
+ if (minutes < 0) clearInterval(interval);
+ seconds = (seconds < 0) ? 59 : seconds;
+ seconds = (seconds < 10) ? '0' + seconds : seconds;
+ 
 }
    
    
